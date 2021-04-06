2486906c7c5cb81154f4c4693cc1a4f748f3ec00
Halbard <HalbardHobby@users.noreply.github.com>
2021-04-05 22:32:40 -0500

    ⚡ Migrated to Capacitor

--- src/index.js
+++ src/index.js
@@ -4,22 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const renderReactDom = () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-
-if (window.cordova) {
-  document.addEventListener('deviceready', () => {
-    renderReactDom();
-  }, false)
-} else {
-  renderReactDom();
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
