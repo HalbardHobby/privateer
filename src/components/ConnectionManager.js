d31f80b378242f80d7420728704805c8591a0864
Halbard <HalbardHobby@users.noreply.github.com>
2021-04-07 19:09:18 -0500

    ⚙ Created connection manager

---
+++ src/components/ConnectionManager.js
@@ -0,0 +1,17 @@
+
+const stunConfig = {
+  iceServers: [{
+    urls: [
+      "stun:stun.l.google.com:19302",
+      "stun:stun1.l.google.com:19302",
+      "stun:stun2.l.google.com:19302",
+      "stun:stun3.l.google.com:19302",
+      "stun:stun4.l.google.com:19302",
+    ],
+  }],
+  iceCandidatePoolSize: 10,
+}
+
+const connection = new RTCPeerConnection(stunConfig);
+
+export default connection;
